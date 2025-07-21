c512eaaa7b797186f86f245de3588e463a02e89b
anytodo292 <anything.todo292@gmail.com>
2025-07-21 04:50:40 +0200

    added admin

---
+++ admin/assets/usePagination-BG5-2Wh0.js
@@ -0,0 +1,1 @@
+import{V as t}from"./vue-BMMZpZdF.js";const r={total:0,currentPage:1,pageSizes:[10,20,50],pageSize:10,layout:"total, sizes, prev, pager, next, jumper"};function s(n={}){const e=t({...r,...n});return{paginationData:e,handleCurrentChange:a=>{e.currentPage=a},handleSizeChange:a=>{e.pageSize=a}}}export{s as u};

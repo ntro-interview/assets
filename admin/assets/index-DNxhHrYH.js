52acc17c5bce0ae0c804f59cccc8fd058d99ee77
anytodo292 <anything.todo292@gmail.com>
2026-03-29 21:01:45 +0200

    made changes

--- admin/assets/index-DNxhHrYH.js
+++
@@ -1,1 +0,0 @@
-import{t as i}from"./index-QQMVn6eO.js";function e(t){return i({url:"/admin/statistics/interview",method:"post",data:t})}function a(t){return i({url:"/admin/statistics/plan",method:"post",data:t})}export{a,e as g};

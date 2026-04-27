41b163a00c246c725ea1587e97395f93c7db21cb
anytodo292 <anything.todo292@gmail.com>
2026-04-27 09:59:56 +0200

    made changes

--- admin/assets/index-ChuzlWLI.js
+++
@@ -1,1 +0,0 @@
-import{t as i}from"./index-25f1Knxs.js";function e(t){return i({url:"/admin/statistics/interview",method:"post",data:t})}function a(t){return i({url:"/admin/statistics/plan",method:"post",data:t})}export{a,e as g};

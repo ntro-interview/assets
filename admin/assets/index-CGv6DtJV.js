4cf32acdffdd4778192a1dbfa8cebb217399765e
anytodo292 <anything.todo292@gmail.com>
2026-04-28 04:51:24 +0200

    made changes

--- admin/assets/index-CGv6DtJV.js
+++
@@ -1,1 +0,0 @@
-import{t as i}from"./index-BbQ8x9a6.js";function e(t){return i({url:"/admin/statistics/interview",method:"post",data:t})}function a(t){return i({url:"/admin/statistics/plan",method:"post",data:t})}export{a,e as g};

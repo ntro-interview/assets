c551a8effee58862952756e105a2040504a38882
anytodo292 <anything.todo292@gmail.com>
2026-05-09 04:51:41 +0200

    made changes

--- admin/assets/index-D-CuyvV-.js
+++
@@ -1,1 +0,0 @@
-import{t as i}from"./index-BvE7dutL.js";function e(t){return i({url:"/admin/statistics/interview",method:"post",data:t})}function a(t){return i({url:"/admin/statistics/plan",method:"post",data:t})}export{a,e as g};

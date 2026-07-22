9da76454c11385b481e3f036df692d08fa834737
anytodo292 <anything.todo292@gmail.com>
2026-07-22 18:25:58 +0100

    made changes

---
+++ admin/assets/index-BuAyPTvM.js
@@ -0,0 +1,1 @@
+import{p as i}from"./index-B4v4C0y8.js";function e(t){return i({url:"/admin/statistics/interview",method:"post",data:t})}function a(t){return i({url:"/admin/statistics/plan",method:"post",data:t})}export{a,e as g};

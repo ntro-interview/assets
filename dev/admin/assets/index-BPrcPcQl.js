57bd9863cec82dc7bd993f2f1c660d38548f40dc
anytodo292 <anything.todo292@gmail.com>
2026-07-29 03:09:08 +0100

    made channges

---
+++ dev/admin/assets/index-BPrcPcQl.js
@@ -0,0 +1,1 @@
+import{p as i}from"./index-DWbYcbN1.js";function e(t){return i({url:"/admin/statistics/interview",method:"post",data:t})}function a(t){return i({url:"/admin/statistics/plan",method:"post",data:t})}export{a,e as g};

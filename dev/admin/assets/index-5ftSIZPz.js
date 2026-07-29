1cfb4a0ac8789de5e341c64d1d2edabd2ffa7395
anytodo292 <anything.todo292@gmail.com>
2026-07-29 03:01:46 +0100

    made changes

--- dev/admin/assets/index-5ftSIZPz.js
+++
@@ -1,1 +0,0 @@
-import{p as i}from"./index-2bwZe0r4.js";function e(t){return i({url:"/admin/statistics/interview",method:"post",data:t})}function a(t){return i({url:"/admin/statistics/plan",method:"post",data:t})}export{a,e as g};
